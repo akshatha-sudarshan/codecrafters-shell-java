c64dbe9dddd87b31ab8eb96af34f10e9fa7a42a5
akshatha <akshathas@bel.co.in>
2025-12-04 10:51:56 +0530

    3rd stage commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-         System.out.print("$ ");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input+": command not found");
-        scanner.close();
+        for (; ; ) {
+            System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+            scanner.close();
+        }
+
     }
 }
