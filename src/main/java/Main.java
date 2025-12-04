113a947274de28f88a272f407d42ca88b5866302
akshatha <akshathas@bel.co.in>
2025-12-04 12:07:06 +0530

    5th stage commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,17 @@ public class Main {
             if (input.equals("exit")) {
                 break;
             }
+            String[] inputArray = input.split(" ");
+            if(inputArray[0].equals("echo")){
+                for(int i = 1; i < inputArray.length; i++){
+                    System.out.print(inputArray[i]);
+                    if(i != inputArray.length - 1){
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+                continue;
+            }
             System.out.println(input + ": command not found");
 //            scanner.close();
         }
