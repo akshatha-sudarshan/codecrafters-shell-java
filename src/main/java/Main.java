22d39f6159d54b7366d987fd52d21a33359dc89c
akshatha <akshathas@bel.co.in>
2025-12-08 11:40:11 +0530

    8th stage commit testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -137,6 +137,7 @@ public class Main {
             // 4. Wait for the process to exit and get the exit code
             int exitCode = process.waitFor();
             System.out.println(output.toString());
+            return;
 
         } catch (IOException e) {
             System.err.println("Error executing command: " + e.getMessage());
